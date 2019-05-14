e438650151226feeb5dec10e58a8c5c86c401066
Fayaz aralikatti <fayazara@gmail.com>
2019-05-14 23:36:43 +0530

    added pagination

--- src/components/Listing.vue
+++ src/components/Listing.vue
@@ -3,57 +3,68 @@
     <div v-if="loading" class="loading">
       <box-icon name="loader-alt" size="lg" animation="spin" color="#c8c8c8"></box-icon>
     </div>
-    <ul v-else>
-      <li v-for="(car, index) in cars" :key="index">
-        <div>
-          <div class="columns">
-            <div class="column is-one-quarter has-text-centered">
-              <img class="carImg" :src="car.photo" alt>
-            </div>
-            <div class="column">
-              <div class="columns">
-                <div class="column">
-                  <div class="marginBottom1">
-                    <p class="title">{{car.name}}</p>
-                    <p class="subtitle">
-                      <box-icon name="map" type="solid" color="#5a5a5a"></box-icon>
-                      &nbsp;{{car.location}}
-                    </p>
+    <div v-else>
+      <ul>
+        <li v-for="(car, index) in displayedCars" :key="index">
+          <div>
+            <div class="columns">
+              <div class="column is-one-quarter has-text-centered">
+                <img class="carImg" :src="car.photo" alt>
+              </div>
+              <div class="column">
+                <div class="columns">
+                  <div class="column">
+                    <div class="marginBottom1">
+                      <p class="title">{{car.name}}</p>
+                      <p class="subtitle">
+                        <box-icon name="map" type="solid" color="#5a5a5a"></box-icon>
+                        &nbsp;{{car.location}}
+                      </p>
+                    </div>
+
+                    <div class="columns is-mobile has-text-centered icons">
+                      <div class="column is-narrow">
+                        <p>
+                          <box-icon color="#c8c8c8" name="group" type="solid"></box-icon>
+                        </p>
+                        <p>Seats: {{car.seats}}</p>
+                      </div>
+                      <div class="column is-narrow">
+                        <p>
+                          <box-icon color="#c8c8c8" name="cog" type="solid"></box-icon>
+                        </p>
+                        <p>Type: {{car.transmission}}</p>
+                      </div>
+                      <div class="column is-narrow">
+                        <p>
+                          <box-icon color="#c8c8c8" name="gas-pump" type="solid"></box-icon>
+                        </p>
+                        <p>Fuel: {{car.fuel_Type}}</p>
+                      </div>
+                    </div>
                   </div>
 
-                  <div class="columns is-mobile has-text-centered icons">
-                    <div class="column is-narrow">
-                      <p>
-                        <box-icon color="#c8c8c8" name="group" type="solid"></box-icon>
-                      </p>
-                      <p>Seats: {{car.seats}}</p>
-                    </div>
-                    <div class="column is-narrow">
-                      <p>
-                        <box-icon color="#c8c8c8" name="cog" type="solid"></box-icon>
-                      </p>
-                      <p>Type: {{car.transmission}}</p>
-                    </div>
-                    <div class="column is-narrow">
-                      <p>
-                        <box-icon color="#c8c8c8" name="gas-pump" type="solid"></box-icon>
-                      </p>
-                      <p>Fuel: {{car.fuel_Type}}</p>
-                    </div>
+                  <div class="column is-narrow has-text-centered pricesection">
+                    <p class="is-size-4">₹ {{car.price}}</p>
+                    <br>
+                    <button class="button is-fullwidth is-medium is-success">Book Now</button>
                   </div>
-                </div>
-
-                <div class="column is-narrow has-text-centered pricesection">
-                  <p class="is-size-4">₹ {{car.price}}</p>
-                  <br>
-                  <button class="button is-fullwidth is-medium is-success">Book Now</button>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </li>
-    </ul>
+        </li>
+      </ul>
+      <div class="buttons has-addons">
+        <span
+          class="button"
+          v-for="(pageNumber, key) in pages"
+          @click="page = pageNumber"
+          :key="key"
+          :class="{'is-primary':page == pageNumber}"
+        >{{pageNumber}}</span>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -62,6 +73,7 @@ import axios from "axios";
 export default {
   data() {
     return {
+      selectedPage: undefined,
       loading: false,
       cars: [],
       apiUrl: "https://api.sheety.co/311576ae-321a-43e3-9a5b-61b3ac373d85",
@@ -69,6 +81,26 @@ export default {
       perPage: 6,
       pages: []
     };
+  },
+  methods: {
+    countPages() {
+      let numberOfPages = Math.ceil(this.cars.length / this.perPage);
+      for (var i = 1; i <= numberOfPages; i++) {
+        this.pages.push(i);
+      }
+    },
+    paginate(cars) {
+      let page = this.page;
+      let perPage = this.perPage;
+      let from = page * perPage - perPage;
+      let to = page * perPage;
+      return cars.slice(from, to);
+    }
+  },
+  watch: {
+    cars() {
+      this.countPages();
+    }
   },
   mounted() {
     this.loading = true;
@@ -84,6 +116,11 @@ export default {
       .catch(error => {
         this.loading = true;
       });
+  },
+  computed: {
+    displayedCars() {
+      return this.paginate(this.cars);
+    }
   }
 };
 </script>
@@ -91,7 +128,7 @@ export default {
 
 <style scoped>
 .loading {
-  height: 100vh;
+  height: 75vh;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -118,14 +155,15 @@ export default {
   width: 200px;
 }
 
+.arrow {
+  width: 15px !important;
+}
+
 li {
   box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
     0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2);
   padding: 1.5rem;
   border-radius: 4px;
-}
-
-li:not(:last-child) {
   margin-bottom: 1.2rem;
 }
 
@@ -141,6 +179,10 @@ li:not(:last-child) {
   .subtitle {
     justify-content: center;
   }
+}
+
+.buttons {
+  justify-content: center;
 }
 </style>
 
